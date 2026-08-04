6ad15b277163150a8cae818ca5a3321c4bb3d158
Yasyf Mohamedali <yasyf@poetic.com>
2026-08-04 02:40:55 -0700

    test: delete cli helpers no test has called since the daemonkit cutover
    seedAccounts, uninstallCmd, and testAccountConfigDir lost their last
    callers when the service suite moved onto the daemonkit seams, and dead
    fixtures invite a future test to be written against them rather than
    against the shape production actually produces. They are three of the
    repo-wide lint failures CI is currently red on.

--- internal/cli/test_paths_test.go
+++ internal/cli/test_paths_test.go
@@ -1,20 +1,11 @@
 package cli
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/yasyf/cc-pool/internal/pool"
 )
-
-func testAccountConfigDir(id int) string {
-	configDir, err := pool.AccountConfigDir(fmt.Sprintf("%032x", id))
-	if err != nil {
-		panic(err)
-	}
-	return configDir
-}
 
 func testFileProviderPublicPath(id int) string {
 	home, err := os.UserHomeDir()
